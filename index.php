0f5762d542b53bd70b0ecd7cd8b5b9e54a0acae6
aacheng17 <aacheng17@gmail.com>
2021-06-23 18:17:31 -0400

    Implement attackError

--- index.php
+++ index.php
@@ -132,9 +132,9 @@ function attackError($unitA, $unitD, $spaces, $weather) {
 }
 
 function calc($unitA, $healthA, $terrainA, $critA, $unitD, $healthD, $terrainD, $critD, $spaces, $weather) {
-  $attackErr = attackError($unitA, $unitD, $spaces, $weather;
-  if (attackErr) != 0) {
-    return array(attackErr => 100);
+  $attackErr = attackError($unitA, $unitD, $spaces, $weather);
+  if ($attackErr) != 0) {
+    return array($attackErr => 100);
   }
   if ($healthA <= 0) return array(0 => 100);
   global $damageMatrix, $unitCrits, $terrainDefenses;
@@ -178,9 +178,9 @@ function calc($unitA, $healthA, $terrainA, $critA, $unitD, $healthD, $terrainD, 
 }
 
 function calcCounterattack($unitA, $healthA, $terrainA, $critA, $unitD, $healthD, $terrainD, $critD, $weather, $spaces, $attackResults) {
-  $attackErr = attackError($unitA, $unitD, $spaces, $weather;
-  if (attackErr) != 0) {
-    return array(attackErr => 100);
+  $attackErr = attackError($unitA, $unitD, $spaces, $weather);
+  if ($attackErr) != 0) {
+    return array($attackErr => 100);
   }
   if ($healthA <= 0) return array(0 => 100);
   $ret = array();
