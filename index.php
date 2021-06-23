3042d32abec0a8f135a89f8f82de62bf7e0d8434
aacheng17 <aacheng17@gmail.com>
2021-06-23 16:38:37 -0400

    Calc randmod testing

--- index.php
+++ index.php
@@ -138,7 +138,10 @@ function calc($unitA, $healthA, $terrainA, $critA, $unitD, $healthD, $terrainD, 
   $probs = array();
   foreach(range(0, count($rands)-2) as $i) {
     $prob = $rands[$i+1] - $rands[$i] * 10;
-    if ($prob > 0) array_push($probs, $prob);
+    if ($prob > 0) {
+      echo $prob;
+      array_push($probs, $prob);
+    }
   }
   foreach($rands as $rand) {
     echo $rand . '<br/>';
